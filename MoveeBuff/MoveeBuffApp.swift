4c1cd38c0a4c9b61c06d294a1b6be5b42610fa9c
Joe Vargas <joeavargas@outlook.com>
2021-07-18 01:42:49 -0500

    skeleton project

---
+++ MoveeBuff/MoveeBuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveeBuffApp.swift
+//  MoveeBuff
+//
+//  Created by Joe Vargas on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct MoveeBuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
