e95dfbd8e01d3cafc2820c19b8e68bb2e11a3130
Joe Vargas <joeavargas@outlook.com>
2021-07-18 07:32:17 -0500

    Movie model built

---
+++ MoveeBuff/Model/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  MoveeBuff
+//
+//  Created by Joe Vargas on 7/18/21.
+//
+
+import Foundation
+
+struct MovieResponse: Decodable {
+    let results: [Movie]
+}
+
+struct Movie: Decodable {
+    let id: Int
+    let title: String
+    let backdropPath: String?
+    let posterPath: String?
+    let overview: String
+    let voteAverage: Double
+    let voteCount: Int
+    let runtime: Int?
+}
